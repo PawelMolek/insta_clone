b1d05277cb021cf64b97d1f3ca97a688aec78172
PawelMolek <pawel.molek@gmail.com>
2022-03-01 12:29:23 +0100

    some visual updates and logout option configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
