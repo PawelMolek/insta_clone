527411ad0a8d37fe77170d73a73256796d581420
PawelMolek <pawel.molek@gmail.com>
2022-03-01 11:11:26 +0100

    Updated gem, routes, home page

---
+++ app/javascript/application.js
@@ -0,0 +1,10 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// import "@hotwired/turbo-rails"
+// import "controllers"
+//= require jquery_ujs
+//= require turbolinks
+//= require popper
+//= require jquery3
+//= require bootstrap-sprockets
+//= require bootstrap
+//= require_tree .
